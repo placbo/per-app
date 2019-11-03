7e259279c4b74aa08be557d70aace3db9bf925e5
pcb <perbjester@gmail.com>
2019-11-03 20:02:20 +0100

    added styled components for MUI

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,15 @@ import React, { useState } from 'react';
 import Axios from 'axios';
 import './App.css';
 import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+import NoSsr from '@material-ui/core/NoSsr';
+import { create } from 'jss';
+import { StylesProvider, jssPreset } from '@material-ui/core/styles';
+
+const jss = create({
+  ...jssPreset(),
+  insertionPoint: 'jss-insertion-point',
+});
 
 const App: React.FC = () => {
   const MainContainer = styled.div`
@@ -15,16 +24,21 @@ const App: React.FC = () => {
     color: white;
   `;
 
-  const Button = styled.button`
+  const ButtonContainer = styled.div`
+    display: flex;
+    height: 100px;
+  `;
+
+  const MyButton = styled.button`
     color: papayawhip;
     border-radius: 3px;
     background-color: palevioletred;
     border: none;
-    padding: 16px 32px;
+    padding: 1rem 2rem;
     text-align: center;
     text-decoration: none;
     display: inline-block;
-    font-size: 16px;
+    font-size: 1rem;
     margin: 1rem;
     transition-duration: 0.4s;
     cursor: pointer;
@@ -33,12 +47,16 @@ const App: React.FC = () => {
     }
   `;
 
-  const ButtonContainer = styled.div`
-    display: flex;
+  const OtherButton = styled(MyButton)`
+    background-color: tomato;
   `;
 
-  const OtherButton = styled(Button)`
-    background-color: tomato;
+  const StyledMUIButton = styled(Button)`
+    color: black;
+    padding: 1rem 2rem;
+    background-color: pink;
+    margin: 1rem;
+    height: 50px;
   `;
 
   const Header = styled.div`
@@ -72,22 +90,38 @@ const App: React.FC = () => {
   };
 
   return (
-    <MainContainer>
-      <Header>PER-APP </Header>
-      <ButtonContainer>
-        <Button data-cy="button1" onClick={handleClick}>
-          What's my name?
-        </Button>
-        <OtherButton data-cy="button2" onClick={handleClick}>
-          What's my name?
-        </OtherButton>
-      </ButtonContainer>
-      {error ? (
-        <Error data-cy="error">{error}</Error>
-      ) : (
-        name && <div data-cy="result">{showSpinner ? 'laster data...' : `Name: ${name}`}</div>
-      )}
-    </MainContainer>
+    //<StylesProvider injectFirst>
+    <StylesProvider jss={jss}>
+      <NoSsr>
+        <MainContainer>
+          <Header>PER-APP </Header>
+          <ButtonContainer>
+            <MyButton data-cy="button1" onClick={handleClick}>
+              Styled Comp-button
+            </MyButton>
+            <OtherButton data-cy="button2" onClick={handleClick}>
+              Styled Comp-button based on first button
+            </OtherButton>
+          </ButtonContainer>
+
+          <ButtonContainer>
+            <Button variant="contained" color="primary">
+              MUI-Button
+            </Button>
+
+            <StyledMUIButton variant="contained" color="primary">
+              styled MUI-Button
+            </StyledMUIButton>
+          </ButtonContainer>
+
+          {error ? (
+            <Error data-cy="error">{error}</Error>
+          ) : (
+            name && <div data-cy="result">{showSpinner ? 'laster data...' : `Name: ${name}`}</div>
+          )}
+        </MainContainer>
+      </NoSsr>
+    </StylesProvider>
   );
 };
 
