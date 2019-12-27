930baca15e2fc89f76f0d69b55ef569a527d1912
pcb <perbjester@gmail.com>
2019-12-27 13:03:17 +0100

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Axios from 'axios';
 import './App.css';
 import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
-import { StylesProvider} from '@material-ui/core/styles';
+import {StylesProvider} from '@material-ui/core/styles';
 
 
 const App: React.FC = () => {
-  const MainContainer = styled.div`
+
+    const MainContainer = styled.div`
     background-color: #282c34;
     min-height: 100vh;
     display: flex;
@@ -18,7 +19,7 @@ const App: React.FC = () => {
     color: white;
   `;
 
-  const ButtonContainer = styled.div`
+    const ButtonContainer = styled.div`
     display: flex;
     height: 100px;
   `;
@@ -45,7 +46,7 @@ const App: React.FC = () => {
     background-color: tomato;
   `;
 
-  const StyledMUIButton = styled(Button)`
+    const StyledMUIButton = styled(Button)`
     color: black;
     padding: 1rem 2rem;
     background-color: pink;
@@ -53,41 +54,41 @@ const App: React.FC = () => {
     height: 50px;
   `;
 
-  const Header = styled.div`
+    const Header = styled.div`
     font-size: 2rem;
     margin-bottom: 1rem;
   `;
 
-  const Error = styled.div`
+    const Error = styled.div`
     font-weight: bold;
     color: darkred;
     padding: 1rem;
   `;
 
-  const [name, setName] = useState('');
-  const [error, setError] = useState('');
-  const [showSpinner, setshowSpinner] = useState(false);
+    const [name, setName] = useState('');
+    const [error, setError] = useState('');
+    const [showSpinner, setshowSpinner] = useState(false);
 
-  const handleClick = (event: React.MouseEvent) => {
-    setshowSpinner(true);
-    Axios.get('http://echo.jsontest.com/name/Per')
-      .then(response => {
-        setTimeout(() => {
-          setName(response.data.name);
-          setshowSpinner(false);
-        }, 1000);
-      })
-      .catch(error => {
-        setError(error.message);
-        setshowSpinner(false);
-      });
-  };
+    const handleClick = (event: React.MouseEvent) => {
+        setshowSpinner(true);
+        Axios.get('http://echo.jsontest.com/name/Per')
+            .then(response => {
+                setTimeout(() => {
+                    setName(response.data.name);
+                    setshowSpinner(false);
+                }, 1000);
+            })
+            .catch(error => {
+                setError(error.message);
+                setshowSpinner(false);
+            });
+    };
 
-  return (
-    <StylesProvider injectFirst>
-        <MainContainer>
+    return (
+        <StylesProvider injectFirst>
+            <MainContainer>
           <Header>PER-APP </Header>
-          <ButtonContainer>
+                <ButtonContainer>
             <MyButton data-cy="button1" onClick={handleClick}>
               Styled Comp-button
             </MyButton>
@@ -103,17 +104,16 @@ const App: React.FC = () => {
 
             <StyledMUIButton variant="contained" color="primary">
               styled MUI-Button
-            </StyledMUIButton>
-          </ButtonContainer>
-
-          {error ? (
-            <Error data-cy="error">{error}</Error>
-          ) : (
-            name && <div data-cy="result">{showSpinner ? 'laster data...' : `Name: ${name}`}</div>
-          )}
-        </MainContainer>
-    </StylesProvider>
-  );
+                    </StyledMUIButton>
+                </ButtonContainer>
+                {error ? (
+                    <Error data-cy="error">{error}</Error>
+                ) : (
+                    name && <div data-cy="result">{showSpinner ? 'laster data...' : `Name: ${name}`}</div>
+                )}
+            </MainContainer>
+        </StylesProvider>
+    );
 };
 
 export default App;
